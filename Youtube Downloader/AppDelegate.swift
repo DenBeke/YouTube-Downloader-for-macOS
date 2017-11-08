85bf093ce77d44ce81eec97db0a29825d24c883e
MathiasB <git@denbeke.be>
2017-11-08 17:20:48 +0100

    Popover

--- Youtube Downloader/AppDelegate.swift
+++ Youtube Downloader/AppDelegate.swift
@@ -11,16 +11,48 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
+    let popover = NSPopover()
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        
+        if let button = statusItem.button {
+            button.image = NSImage(named:NSImage.Name("Menu"))
+            button.action = #selector(togglePopover(_:))
+        }
+        popover.contentViewController = DownloadViewController.freshController()
+        popover.behavior = .transient
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
+    
+    
+    
+    @objc func togglePopover(_ sender: Any?) {
+        if popover.isShown {
+            closePopover(sender: sender)
+        } else {
+            showPopover(sender: sender)
+        }
+    }
+    
+    func showPopover(sender: Any?) {
+        if let button = statusItem.button {
+            popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+            NSApplication.shared.activate(ignoringOtherApps: true)
+        }
+    }
+    
+    func closePopover(sender: Any?) {
+        popover.performClose(sender)
+    }
+    
 
 }
+
+
 
---
+++ Youtube Downloader/DownloadViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DownloadViewController.swift
+//  Youtube Downloader
+//
+//  Created by Mathias Beke on 8/11/17.
+//  Copyright © 2017 Mathias Beke. All rights reserved.
+//
+
+import Cocoa
+
+class DownloadViewController: NSViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
+
+
+extension DownloadViewController {
+    // MARK: Storyboard instantiation
+    static func freshController() -> DownloadViewController {
+        //1.
+        let storyboard = NSStoryboard(name: NSStoryboard.Name(rawValue: "Main"), bundle: nil)
+        //2.
+        let identifier = NSStoryboard.SceneIdentifier(rawValue: "DownloadViewController")
+        //3.
+        guard let viewcontroller = storyboard.instantiateController(withIdentifier: identifier) as? DownloadViewController else {
+            fatalError("Why cant i find DownloadViewController? - Check Main.storyboard")
+        }
+        return viewcontroller
+    }
+}
+

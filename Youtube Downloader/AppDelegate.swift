993106c052183a3bc384a37ce5b4703b69b55ef0
MathiasB <git@denbeke.be>
2017-11-08 16:42:06 +0100

    Initial Commit

---
+++ Youtube Downloader/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Youtube Downloader
+//
+//  Created by Mathias Beke on 8/11/17.
+//  Copyright © 2017 Mathias Beke. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Youtube Downloader/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Youtube Downloader
+//
+//  Created by Mathias Beke on 8/11/17.
+//  Copyright © 2017 Mathias Beke. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

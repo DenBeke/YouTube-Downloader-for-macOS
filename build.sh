ed05f2c953280b25192e687be0ec7445d7e42325
MathiasB <git@denbeke.be>
2024-07-16 22:20:06 +0200

    Fixed universal build

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ DIR="build"
 
 rm -rf $DIR/*
 
-xcodebuild -project Youtube\ Downloader.xcodeproj -scheme "Youtube Downloader" -config Release -archivePath ./$DIR/Youtube\ Downloader.xcarchive archive
+xcodebuild -project Youtube\ Downloader.xcodeproj ONLY_ACTIVE_ARCH=NO -scheme "Youtube Downloader" -config Release -archivePath ./$DIR/Youtube\ Downloader.xcarchive archive
 
 xcodebuild -archivePath ./$DIR/Youtube\ Downloader.xcarchive -exportArchive -exportPath ./$DIR/ -exportOptionsPlist exportOptions.plist
